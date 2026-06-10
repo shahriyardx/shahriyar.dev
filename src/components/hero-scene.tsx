c0fd54b421582394dfd33c93c064cbf9b255db10
Shahriyar <mdshahriyaralam552@gmail.com>
2026-06-11 02:33:30 +0600

    fix: keep 3D shapes within hero section bounds

--- src/components/hero-scene.tsx
+++ src/components/hero-scene.tsx
@@ -15,18 +15,18 @@ function Shapes() {
   })
 
   return (
-    <group ref={group} position={[1.5, -0.5, 0]}>
+    <group ref={group} position={[0.5, -0.2, 0]}>
       <mesh position={[0, 0, 0]}>
-        <octahedronGeometry args={[0.6, 0]} />
-        <meshNormalMaterial wireframe transparent opacity={0.3} />
+        <octahedronGeometry args={[0.5, 0]} />
+        <meshNormalMaterial wireframe transparent opacity={0.35} />
       </mesh>
-      <mesh position={[1.2, 0.8, -0.5]}>
-        <icosahedronGeometry args={[0.4, 0]} />
-        <meshNormalMaterial wireframe transparent opacity={0.2} />
-      </mesh>
-      <mesh position={[-0.8, 0.6, 0.5]}>
-        <dodecahedronGeometry args={[0.35, 0]} />
+      <mesh position={[0.6, 0.4, -0.3]}>
+        <icosahedronGeometry args={[0.35, 0]} />
         <meshNormalMaterial wireframe transparent opacity={0.25} />
+      </mesh>
+      <mesh position={[-0.5, 0.3, 0.3]}>
+        <dodecahedronGeometry args={[0.3, 0]} />
+        <meshNormalMaterial wireframe transparent opacity={0.3} />
       </mesh>
     </group>
   )
@@ -35,7 +35,7 @@ function Shapes() {
 export function HeroScene() {
   return (
     <div className="pointer-events-none absolute inset-0 -z-10 hidden md:block">
-      <Canvas camera={{ position: [0, 0, 3.5], fov: 50 }} gl={{ alpha: true }}>
+      <Canvas camera={{ position: [0, 0, 4], fov: 45 }} gl={{ alpha: true }}>
         <Shapes />
       </Canvas>
     </div>
